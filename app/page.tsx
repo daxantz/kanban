5be0086cca860e6276e754bb9565054c966b57f8
daxantz <daxantz@gmail.com>
2025-08-02 17:10:47 -0400

    fetched columns on server instead of getting it from state

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,28 @@
-"use client";
 import Column from "@/components/Column";
 
 import { Button } from "@/components/ui/button";
-import { useBoardContext } from "@/lib/context/BoardContext";
+
+import { prisma } from "@/lib/prisma";
 import Image from "next/image";
 
-export default function Home() {
-  const { state } = useBoardContext();
-
-  if (!state.board.columns)
+type Props = {
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+export default async function Home({ searchParams }: Props) {
+  const boardId = await searchParams?.board;
+  const columns = await prisma.column.findMany({
+    where: {
+      boardId: boardId as string,
+    },
+    include: {
+      tasks: {
+        include: {
+          subtasks: true,
+        },
+      },
+    },
+  });
+  if (columns.length === 0)
     return (
       <div className="flex flex-col items-center px-4 border h-screen justify-center">
         <p className="text-medium-grey font-bold text-center mb-6">
@@ -28,7 +42,7 @@ export default function Home() {
 
   return (
     <div className="px-4 py-6 flex gap-6  overflow-x-scroll bg-light-grey dark:bg-dark-grey">
-      {state.board.columns.map((col) => (
+      {columns.map((col) => (
         <Column column={col} key={col.name} />
       ))}
       <div className="flex flex-col gap-6">
