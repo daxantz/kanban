8593e21d73ee87709724e0180d717f50ef22f883
daxantz <daxantz@gmail.com>
2025-08-06 19:46:58 -0400

    created server actions for deleting boards and createing tasks

--- app/actions/actions.ts
+++ app/actions/actions.ts
@@ -3,11 +3,14 @@
 import { prisma } from "@/lib/prisma";
 import { authOptions } from "@/lib/authOptions";
 import { getServerSession } from "next-auth";
+import { Board } from "@prisma/client";
+import { FullTask } from "@/lib/types";
+import { revalidatePath } from "next/cache";
 
 export async function createBoard(
   prevState: { message: string },
   formData: FormData
-): Promise<{ message: string }> {
+): Promise<{ message: string; board?: Board }> {
   const session = await getServerSession(authOptions);
   const boardName = formData.get("name");
 
@@ -40,7 +43,7 @@ export async function createBoard(
     return { message: "A board with this name already exists" };
   }
 
-  await prisma.board.create({
+  const board = await prisma.board.create({
     data: {
       name: boardName as string,
       ownerId: user.id,
@@ -50,5 +53,57 @@ export async function createBoard(
     },
   });
 
-  return { message: "Board created successfully" };
+  return { message: "Board created successfully", board: board };
+}
+
+export async function deleteBoard(boardId: string) {
+  await prisma.board.delete({
+    where: {
+      id: boardId,
+    },
+  });
+}
+
+// actions.ts
+export async function createTask(
+  prevState: { message: string; task?: FullTask },
+  formData: FormData
+): Promise<{ message: string; task?: FullTask }> {
+  const title = formData.get("title") as string;
+  const description = formData.get("description") as string;
+  const status = formData.get("status") as string;
+  const boardId = formData.get("boardId") as string;
+
+  if (!title || !boardId || !status) {
+    return { message: "Missing required fields" };
+  }
+  const board = await prisma.board.findUnique({
+    where: {
+      id: boardId,
+    },
+  });
+  const column = await prisma.column.findFirst({
+    where: {
+      name: status,
+      boardId: board?.id,
+    },
+  });
+
+  if (!column) {
+    return { message: "Column not found" };
+  }
+
+  const task = await prisma.task.create({
+    data: {
+      title,
+      description,
+      status,
+      columnId: column.id,
+    },
+    include: {
+      subtasks: true,
+    },
+  });
+  revalidatePath("/");
+  return { message: "Task created", task };
 }
